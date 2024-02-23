e73a4cc9ae51b4b9cac4f45235363e813746562b
Brandon <65436229+baej12@users.noreply.github.com>
2024-02-23 13:55:51 -0700

    broken link fix

--- src/components/ServicesPage/ServicesPage.tsx
+++ src/components/ServicesPage/ServicesPage.tsx
@@ -8,7 +8,7 @@ export const ServicesPage = () => {
                 <h3>This page has been taken down for maintenance.</h3> 
                 If you would like to contact me for my services, please use the information provided in the resume or contact
                 me through <a href='https://www.linkedin.com/in/jhbbae/'>LinkedIn</a><br/>
-                Otherwise, please use my <a href='/about'>About</a> page to learn more about me.
+                Otherwise, please use my <a href='/#/about'>About</a> page to learn more about me.
             </p>
         </div>
     </div>
